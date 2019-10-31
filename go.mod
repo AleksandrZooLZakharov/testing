8b514e1969c926a7a18fc3aa55b51ca5af240eb0
Aleksandr Zakharov <zoolgle@gmail.com>
2019-10-31 17:37:01 +0300

    continue testing and learn go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module AleksandrZooLZakharov/testing
 
 go 1.13
+
+require github.com/goestoeleven/go-programming v0.0.0-20181228215051-5519dbb87bc5 // indirect
--- hello.go
+++ hello.go
@@ -1,5 +1,12 @@
-package Hello
+package main
+
+import "fmt"
+
+func main() {
+	Hello()
+}
 
 func Hello() string {
+	fmt.Println("Hello, world from fmt")
 	return "Hello, world."
 }
