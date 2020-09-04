3cc34529fd572b4b0a4a74eda65a1cf3ae5d310c
Aleksandr Zakharov <zoolgle@gmail.com>
2020-09-04 14:38:47 +0300

    continue testing and learn go

--- hello.go
+++ hello.go
@@ -1,13 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
+
+var err error //for storing errors
+type person struct {
+	First string `json:"First"`
+	Last  string `json:"Last"`
+	Age   int    `json:"Age"`
+}
+
+var people []person
 
 func main() {
-	fmt.Println("Hello, world from fmt")
-	foo()
-	fmt.Println("Something there")
+	s := `[{"First":"James","Last":"Bond","Age":32},{"First":"Miss","Last":"Moneypenny","Age":27}]`
+	bs := []byte(s)
+
+	fmt.Printf("%T\t%T\n", s, bs)
+	fmt.Println(s)
+	err = json.Unmarshal(bs, &people)
+	if err != nil {
+		fmt.Printf("Printing a type of error handler: %T\n", err)
+		fmt.Println(err)
+	}
+	fmt.Printf("%+v", people[1])
 }
 
-func foo() {
-	fmt.Println("Hello from fmt from foo() ;)")
-}
+// func (r receiver) identifier(parameters) (return(s)) { code }
